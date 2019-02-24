70e481c9f4fefbd87625a66e6126cccc539e50ba
christos <chrishadjiaslanis@gmail.com>
2019-02-24 17:00:24 +0000

    Finished deployment script and placeholder install script

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,33 @@
+#!/bin/bash
+
 ZONE="europe-west1-b"
 INSTANCE="hacker-cache-vm"
 
 echo "Copying jar to cloud..."
-gcloud compute scp --zone ${ZONE} target/hacker-cache-1.0.0.jar ${INSTANCE}:~/hacker-cache/ \
-	|| echo "Could not copy jar to remote VM. Exiting..."; exit 1;
+gcloud compute scp --zone ${ZONE} target/hacker-cache-1.0.0.jar ${INSTANCE}:~/hacker-cache/
+if [ $? -ne 0 ]	
+then
+	echo "Could not copy jar to remote VM. Exiting..." 
+	exit 1
+fi
 echo "Success."
 
 echo "Copying install script to cloud..."
-gcloud compute scp --zone ${ZONE} install.sh ${INSTANCE}:~/hacker-cache/ \
-	|| echo "Could not copy install script to remote VM. Exiting..."; exit 1;
+gcloud compute scp --zone ${ZONE} install.sh ${INSTANCE}:~/hacker-cache/
+if [ $? -ne 0 ]
+then
+	echo "Could not copy install script to remote VM. Exiting..." 
+	exit 1
+fi
 echo "Success."
 
 echo "Attempting to ssh into remote VM and run install script";
-gcloud compute ssh --zone ${ZONE} ${INSTANCE} --command "cd hacker-cache && ./install.sh" \
-	|| echo "Could not install application remotely. Exiting..."; exit 1;
+gcloud compute ssh --zone ${ZONE} ${INSTANCE} --command "cd hacker-cache && ./install.sh"
+if [ $? -ne 0 ]
+then
+	echo "Could not install application remotely. Exiting..." 
+	exit 1
+fi
 echo "Success."
 
 exit 0;
